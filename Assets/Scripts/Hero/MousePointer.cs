78160c2b4d32e7a9431fc967cde3fcbeb21862fc
Steve Salmond <steve.salmond@smallworlds.com>
2014-04-26 17:07:07 +1200

    Working towards mouse control of player's sword.

---
+++ Assets/Scripts/Hero/MousePointer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MousePointer : MonoBehaviour {
+
+	private Vector3 screen;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	}
+}
