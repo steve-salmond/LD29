06845461c93271cd77d14151753d545552f50c4a
Steve Salmond <steve.salmond@smallworlds.com>
2014-04-27 16:08:24 +1200

    Adding some basic instructions.

---
+++ Assets/Scripts/Camera/BottomLeft.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BottomLeft : MonoBehaviour {
+
+	public Vector2 Offset;
+
+	
+	void LateUpdate () {
+		transform.position = Camera.main.ScreenToWorldPoint(Offset);
+	}
+}
