b3ee13abb6eac7274d1e6432eceb01f54b4aefe6
Steve Salmond <steve.salmond@smallworlds.com>
2014-04-27 15:17:27 +1200

    Added a music loop.

---
+++ Assets/Scripts/Player/BerserkerMusic.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BerserkerMusic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Player.Instance.Beserk)
+			audio.volume = 1;
+		else
+			audio.volume = 0.25f;
+	}
+}
